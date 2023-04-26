963c29aff9ce2e8ac526c6f4a683d5a78d79db33
Edgar Muñoz <edgmuo1085@gmail.com>
2023-04-26 10:30:04 -0500

    se agrega metodo para capturar campos que llegan en la url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, isDevMode } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'verFacturaAda';
+  consultaFactura: string = '';
+
+  ngOnInit(): void {
+    const fact = this.getParameterByName('fact');
+    if (isDevMode()) {
+      console.log('fact: ', fact);
+    }
+    if (fact) {
+      //this.consultaFactura = JSON.parse(window.atob(fact));
+    }
+  }
+
+  getParameterByName(name: string) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    const regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    let url = location.href;
+    url = url.substring(url.indexOf('?fact='), url.length);
+    const results = regex.exec(url);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+  }
 }
